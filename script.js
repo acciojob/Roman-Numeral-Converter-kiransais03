e8cbb700b1fa542bea82f25d8f1b927819ef193f
Guduru Kiran Sai <119746361+kiransais03@users.noreply.github.com>
2023-06-28 12:54:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,100 @@
 function convertToRoman(num) {
   	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
+      '1000':'M', 
+      '500':'D', 
+      '100':'C', 
+      '50':'L', 
+      '10':'X', 
+      '5':'V', 
+      '1':'I',
     };
-
+let romanstr='';
   //your code here
+	while(num>=1) {
+		if(num>=1000)
+		{
+			let rem=num%1000;
+			let addct=parseInt(num/1000);
+			for(let i=1;i<=addct;i++)
+				{
+					romanstr+=obj[1000];
+				}
+			num=rem;
+		} else if(num>=900) {
+			romanstr+='CM';
+			num=num-900;
+		} else if(num>=500)
+		  {
+			let rem=num%500;
+			let addct=parseInt(num/500);
+			for(let i=1;i<=addct;i++)
+				{
+					romanstr+=obj[500];
+				}
+			num=rem;
+		}  else if(num>=400) {
+			romanstr+='CD';
+			num=num-400;
+		} else if(num>=100)
+		  {
+			let rem=num%100;
+			let addct=parseInt(num/100);
+			for(let i=1;i<=addct;i++)
+				{
+					romanstr+=obj[100];
+				}
+			num=rem;
+		} else if(num>=90) {
+			romanstr+='XC';
+			num=num-90;
+		} else if(num>=50)
+		  {
+			let rem=num%50;
+			let addct=parseInt(num/50);
+			for(let i=1;i<=addct;i++)
+				{
+					romanstr+=obj[50];
+				}
+			num=rem;
+		} else if(num>=40) {
+			romanstr+='XL';
+			num=num-40;
+		}  else if(num>=10)
+		  {
+			let rem=num%10;
+			let addct=parseInt(num/10);
+			for(let i=1;i<=addct;i++)
+				{
+					romanstr+=obj[10];
+				}
+			num=rem;
+		} else if(num==9) {
+			romanstr+='IX';
+			num=0;
+		} else if(num>=5)
+		  {
+			let rem=num%5;
+			let addct=parseInt(num/5);
+			for(let i=1;i<=addct;i++)
+				{
+					romanstr+=obj[5];
+				}
+			num=rem;
+		} else if(num==4) {
+			romanstr+='IV';
+			num=0;
+		} else if(num>=1)
+		  {
+			let rem=num%1;
+			let addct=parseInt(num/1);
+			for(let i=1;i<=addct;i++)
+				{
+					romanstr+=obj[1];
+				}
+			num=rem;
+		}
+	}
+  return romanstr;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
